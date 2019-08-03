dc22e7c6f4b0c90e56dbabe5ef17572c98865977
Luc Engelen <luc@kabisa.nl>
2019-08-03 20:20:20 +0200

    Refactor echo service

--- src/main/java/nl/kabisa/vertx/tcp/ScreamingEchoServiceVerticle.java
+++ src/main/java/nl/kabisa/vertx/tcp/ScreamingEchoServiceVerticle.java
@@ -11,6 +11,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServerOptions;
+import io.vertx.core.net.NetSocket;
 
 public class ScreamingEchoServiceVerticle extends AbstractVerticle {
 
@@ -20,6 +21,47 @@ public class ScreamingEchoServiceVerticle extends AbstractVerticle {
     private static final byte[] OK = { 1 };
     private static final Buffer FAILURE = Buffer.buffer(new byte[] { 0 });
 
+    private Future<Boolean> validateToken(String token) {
+        Future<Boolean> future = Future.future();
+
+        vertx.sharedData().<String, Boolean> getAsyncMap(AUTHENTICATED_CLIENTS_MAP, asyncMap -> {
+            if (asyncMap.succeeded()) {
+                var map = asyncMap.result();
+                map.get(token, asyncAuthenticated -> {
+                    if (asyncAuthenticated.succeeded()) {
+                        future.complete(asyncAuthenticated.result());
+                    } else {
+                        LOGGER.error("Unable to get value for {} from map {}", token, AUTHENTICATED_CLIENTS_MAP, asyncAuthenticated.cause());
+                        future.fail(asyncAuthenticated.cause());
+                    }
+                });
+            } else {
+                LOGGER.error("Unable to get map {}", AUTHENTICATED_CLIENTS_MAP, asyncMap.cause());
+                future.fail(asyncMap.cause());
+            }
+        });
+
+        return future;
+    }
+
+    private void handleRequest(NetSocket socket, Buffer buffer) {
+        LOGGER.info("Received buffer: {}", buffer);
+
+        var id = buffer.getString(0, 36);
+        validateToken(id).setHandler(asyncValidationResult -> {
+            if (asyncValidationResult.succeeded()) {
+                var isValid = asyncValidationResult.result();
+                if (Boolean.TRUE.equals(isValid)) {
+                    socket.write(Buffer.buffer(Bytes.concat(OK, buffer.getString(36, buffer.length()).toUpperCase().getBytes())));
+                } else {
+                    socket.write(NOK);
+                }
+            } else {
+                socket.write(FAILURE);
+            }
+        });
+    }
+
     @Override
     public void start(Future<Void> startFuture) {
         LOGGER.info("Starting");
@@ -28,31 +70,8 @@ public class ScreamingEchoServiceVerticle extends AbstractVerticle {
         var netServer = vertx.createNetServer(options);
 
         netServer.connectHandler(socket ->
-                socket.handler(buffer -> {
-                    LOGGER.info("Received buffer: {}", buffer);
-                    var id = buffer.getString(0, 36);
-                    vertx.sharedData().<String, Boolean> getAsyncMap(AUTHENTICATED_CLIENTS_MAP, asyncMap -> {
-                        if (asyncMap.succeeded()) {
-                            var map = asyncMap.result();
-                            map.get(id, asyncAuthenticated -> {
-                                if (asyncAuthenticated.succeeded()) {
-                                    var authenticated = asyncAuthenticated.result();
-                                    if (Boolean.TRUE.equals(authenticated)) {
-                                        socket.write(Buffer.buffer(Bytes.concat(OK, buffer.getString(36, buffer.length()).toUpperCase().getBytes())));
-                                    } else {
-                                        socket.write(NOK);
-                                    }
-                                } else {
-                                    LOGGER.error("Unable to get value for {} from map {}", id, AUTHENTICATED_CLIENTS_MAP, asyncAuthenticated.cause());
-                                    socket.write(FAILURE);
-                                }
-                            });
-                        } else {
-                            LOGGER.error("Unable to get map {}", AUTHENTICATED_CLIENTS_MAP, asyncMap.cause());
-                            socket.write(FAILURE);
-                        }
-                    });
-                }));
+                socket.handler(buffer ->
+                        handleRequest(socket, buffer)));
 
         netServer.listen(ar -> {
             if (ar.succeeded()) {
